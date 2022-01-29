5380ac7f08238cd5c73cea28b9646577843a9459
Hossein Hazratgholizadeh <hossein.hazratgholizade@gmail.com>
2022-01-30 01:38:38 +0200

    Create Button.css
    Create Button.css and import button to the Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,24 @@
 import { click } from '@testing-library/user-event/dist/click';
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom'
+import { Button } from './Button';
+import './Navbar.css'
 
 
 function Navbar() {
   const [click, setClick]= useState(false);
+  const [button, setButton]= useState(false);
 
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false)
+  const showButton = () => {
+    if (window.innerWidth <= 960){
+      setButton(false);
+    }else {
+      setButton(true);
+    }
+  }
 
   return (
     <div>
@@ -41,8 +51,10 @@ function Navbar() {
                 Sign Up
                 </Link>
             </li>
-
           </ul>
+          {button && <Button buttonStyle='btn--outline' >
+            SIGN UP
+          </Button>}
         </div>
       </nav>
     </div>
