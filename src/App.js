0ca0220aab573a03bf448d86f5244726787501e7
Hossein Hazratgholizadeh <hossein.hazratgholizade@gmail.com>
2022-01-31 06:37:05 +0200

    Create HeroSection and change dependencies
    Create a HeroSection component and css
    Create a Home component
    Change package json to use react-router-dom version 5

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/Home'
 
 
 
 function App() {
   return (
     <>
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path='/' exact />
-      </Routes>
-    </Router>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+        </Switch>
+      </Router>
     </>
   );
 }
