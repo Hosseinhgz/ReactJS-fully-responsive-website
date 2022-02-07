a64feecb748786a454ef67a1fb011ca5791b25d0
Hossein Hazratgholizadeh <hossein.hazratgholizade@gmail.com>
2022-02-07 05:44:55 +0200

    Fix some design Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,26 +5,29 @@ import './Footer.css'
 
 function Footer() {
   return (
-  <div className='footer-container'>
+    <div className='footer-container'>
     <section className='footer-subscription'>
       <p className='footer-subscription-heading'>
-        Join the Adventure newsletter to recieve our best vacation deals
+        Join the Adventure newsletter to receive our best vacation deals
       </p>
       <p className='footer-subscription-text'>
-        Join the Adventure newsletter to recieve our best vacation deals
+        You can unsubscribe at any time.
       </p>
-      <div>
+      <div className='input-areas'>
         <form>
-          <input className='footer-input' type="email" 
-          name="email" 
-          placeholder='Your email' />
-          <Button buttonStyle="btn--outline">Subscribe</Button>
+          <input
+            className='footer-input'
+            name='email'
+            type='email'
+            placeholder='Your Email'
+          />
+          <Button buttonStyle='btn--outline'>Subscribe</Button>
         </form>
       </div>
     </section>
-    <div className='footer-links'>
+    <div class='footer-links'>
       <div className='footer-link-wrapper'>
-        <div className='footer-link-items'>
+        <div class='footer-link-items'>
           <h2>About Us</h2>
           <Link to='/sign-up'>How it works</Link>
           <Link to='/'>Testimonials</Link>
@@ -56,60 +59,60 @@ function Footer() {
           <Link to='/'>Twitter</Link>
         </div>
       </div>
-      <section class='social-media'>
-        <div class='social-media-wrap'>
-          <div class='footer-logo'>
-            <Link to='/' className='social-logo'>
-              HGZ
-              <i class='fab fa-typo3' />
-            </Link>
-          </div>
-          <small class='website-rights'>HGZ © 2022</small>
-          <div class='social-icons'>
-            <Link
-              class='social-icon-link facebook'
-              to='/'
-              target='_blank'
-              aria-label='Facebook'
-            >
-              <i class='fab fa-facebook-f' />
-            </Link>
-            <Link
-              class='social-icon-link instagram'
-              to='/'
-              target='_blank'
-              aria-label='Instagram'
-            >
-              <i class='fab fa-instagram' />
-            </Link>
-            <Link
-              class='social-icon-link youtube'
-              to='/'
-              target='_blank'
-              aria-label='Youtube'
-            >
-              <i class='fab fa-youtube' />
-            </Link>
-            <Link
-              class='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='Twitter'
-            >
-              <i class='fab fa-twitter' />
-            </Link>
-            <Link
-              class='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='LinkedIn'
-            >
-              <i class='fab fa-linkedin' />
-            </Link>
-          </div>
-        </div>
-      </section>
     </div>
+    <section class='social-media'>
+      <div class='social-media-wrap'>
+        <div class='footer-logo'>
+          <Link to='/' className='social-logo'>
+            TRVL
+            <i class='fab fa-typo3' />
+          </Link>
+        </div>
+        <small class='website-rights'>TRVL © 2020</small>
+        <div class='social-icons'>
+          <Link
+            class='social-icon-link facebook'
+            to='/'
+            target='_blank'
+            aria-label='Facebook'
+          >
+            <i class='fab fa-facebook-f' />
+          </Link>
+          <Link
+            class='social-icon-link instagram'
+            to='/'
+            target='_blank'
+            aria-label='Instagram'
+          >
+            <i class='fab fa-instagram' />
+          </Link>
+          <Link
+            class='social-icon-link youtube'
+            to='/'
+            target='_blank'
+            aria-label='Youtube'
+          >
+            <i class='fab fa-youtube' />
+          </Link>
+          <Link
+            class='social-icon-link twitter'
+            to='/'
+            target='_blank'
+            aria-label='Twitter'
+          >
+            <i class='fab fa-twitter' />
+          </Link>
+          <Link
+            class='social-icon-link twitter'
+            to='/'
+            target='_blank'
+            aria-label='LinkedIn'
+          >
+            <i class='fab fa-linkedin' />
+          </Link>
+        </div>
+      </div>
+    </section>
   </div>
   );
 }
