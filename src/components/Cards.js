f6b5ab1427b75f6ff9b3f2eb7e18a5ba0cff8460
Hossein Hazratgholizadeh <hossein.hazratgholizade@gmail.com>
2022-02-07 04:25:39 +0200

    Create Some pages

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -9,7 +9,38 @@ function Cards() {
     <div className='cards__container'>
       <div className='cards__wrapper'>
         <ul className='cards__items'>
-          <CardItem />
+          <CardItem 
+          src='images/img-9.jpg'
+          text='Explore the hidden waterfall deep inside the Amazon Jungle'
+          label='Adventure'
+          path='/services'
+          />
+          <CardItem 
+          src='images/img-6.jpg'
+          text='Explore TOKYO'
+          label='Adventure'
+          path='/services'
+          />
+        </ul>
+        <ul className='cards__items'>
+          <CardItem 
+          src='images/img-7.jpg'
+          text='Explore the real nature in desert night in Egypt'
+          label='Adventure'
+          path='/services'
+          />
+          <CardItem 
+          src='images/img-8.jpg'
+          text='Explore the hidden desert of Iraq'
+          label='Adventure'
+          path='/services'
+          />
+          <CardItem 
+          src='images/img-5.jpg'
+          text='Explore the City of New York'
+          label='Adventure'
+          path='/services'
+          />
         </ul>
       </div>
     </div>
---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Footer() {
+  return (
+  <div className='footer-container'>
+    <section className='footer-subscription'>
+      <p className='footer-subscription-heading'>
+        Join the Adventure newsletter to recieve our best vacation deals
+      </p>
+      <p className='footer-subscription-heading'>
+        Join the Adventure newsletter to recieve our best vacation deals
+      </p>
+    </section>
+  </div>
+  );
+}
+
+export default Footer;
